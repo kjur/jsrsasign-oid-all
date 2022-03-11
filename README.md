8998f230acc27ddbe9faeefc3bbed65b9ef8c880
Kenji Urushima <kenji.urushima@gmail.com>
2022-03-11 18:24:57 +0900

    0.9.1 release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# jsrsasign-oid-all
-jsrsasign add on to add all of registered OID definitions
+[jsrsasign](https://github.com/kjur/jsrsasign/) | [npm](https://www.npmjs.com/package/jsrsasign-oid-all) | [OIDs](https://github.com/kjur/jsrsasign-oid-all/blob/main/lib/index.js) | [Other AddOn](https://github.com/kjur/jsrsasign/wiki/jsrsasign-Add-On)
+
+# jsrsasign-oid-ru
+jsrsasign add-on to add all of registered object identifier definitions.
+
+## How to use add-on
+```JavaScript
+var jsrsasign = require("jsrsasign");
+require("jsrsasign-oid-all").addon(jsrsasign);
+```
+
+## OID definitions
+As for registered OIDs, please see [jsrsasign add-on](https://github.com/kjur/jsrsasign/wiki/jsrsasign-Add-On) wiki page.
+
+## Example
+```JavaScript
+var jsrsasign = require("jsrsasign");
+require("jsrsasign-oid-all").addon(jsrsasign);
+
+...snip...
+
+h = "<<<sample certificate hexadecimal data>>>";
+console.log(JSON.stringify(jsrsasign.ASN1HEX.parse(h), null, " "));
+```
+
+## Reference
+none
---
+++ test/t_oid.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+var rs = require("jsrsasign");
+require('../lib/index.js').addon(rs);
+
+var oids = {
+    "Russian Federation":		"1.2.643",			// from ru
+    "psd2-qcp-web":			"0.4.0.19495.3.1",		// from psd2
+    "jpcomreg-certificatePolicy":	"1.2.392.100300.1.3.4",		// from jpcomreg
+    "jpki-pd-commonName":               "1.2.392.200149.8.5.5.1"	// from jpki
+};
+
+function addon(jsrsasign) {
+    jsrsasign.KJUR.asn1.x509.OID.registerOIDs(oids);
+}
+
+exports.addon = addon;
+
+
+describe("Full OIDs", function() {
+    describe("KJUR.asn1.x509.OID", function() {
+	var OID = rs.KJUR.asn1.x509.OID;
+	var name2oid = OID.name2oid;
+	var equal = assert.equal;
+
+        it('existing sha256', function() {
+            equal("2.16.840.1.101.3.4.2.1", name2oid("sha256"));
+        });
+
+	for (var name in oids) {
+	    it("name2oid " + name,
+	       function() {equal(oids[name], OID.name2oid(name));});
+	}
+
+	for (var name in oids) {
+	    it("oid2name " + name,
+	       function() {equal(name, OID.oid2name(oids[name]));});
+	}
+    });
+});
